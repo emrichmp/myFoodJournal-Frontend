648afa37d6f96ad28a49b041034103f625c6f6fd
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-05-24 18:42:41 -0400

    added tdee to dom as test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar"
+import Tdee from "./Tdee"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Tdee />
     </div>
   );
 }
