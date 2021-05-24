358f0b741fec310578bcf01501d4dc7edd47713c
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-05-24 19:05:49 -0400

    added react-router to package.json, added router components to app.js, updated navbar to be <NavLink> instead of href./

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 //import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar"
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
 import Tdee from "./Tdee"
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Tdee />
+      <HashRouter>
+        <Navbar />
+      </HashRouter>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import { MenuItems } from "./MenuItems"
 import './Navbar.css'
+import { NavLink } from 'react-router-dom'
 
 class Navbar extends React.Component {
-    state = { clicked: false }
-
-    handleClick = () => {
-        this.setState({ clicked: !this.state.clicked })
-    }
-
     render() {
         return (
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">myFoodJournal<i className="fas fa-weight"></i></h1>
-                {/* <div className="menu-icon" onClick={this.handleClick}>
-                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div> */}
-                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                <ul className='nav-menu'>
                     {MenuItems.map((item, index) => {
                         return(
                             <li key={index}>
-                                <a className={item.cName} href={item.url}>
-                                    {item.title}
-                                </a>
+                                <NavLink to={item.url} className={item.cName}>{item.title}</NavLink>
                             </li>
                         )
                     })}
