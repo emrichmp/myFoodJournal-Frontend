5dd28081a1a60bc3b4c5b6895db29e084bb601c3
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-05-24 19:24:08 -0400

    added comp and tested if it qwas showing in dom

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import {
   HashRouter
 } from "react-router-dom";
 import Tdee from "./components/Tdee"
+import Mydiary from './components/myDiary';
 
 function App() {
   return (
     <div className="App">
       <HashRouter>
         <Navbar />
+        <Mydiary />
         <div className="content">
           <Route exact path="/tdee" component={Tdee}/>
         </div>
---
+++ src/components/myDiary.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Mydiary extends React.Component {
+    render () {
+        return(
+            <div>test</div>
+        )
+    }
+}
+
+export default Mydiary
