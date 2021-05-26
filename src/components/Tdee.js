9c45c1dbe042a4c9de2790272e3964a8a7c9433a
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-05-26 18:12:36 -0400

    added name to form elements

--- src/components/Tdee.js
+++ src/components/Tdee.js
@@ -1,26 +1,32 @@
 import React from 'react';
 
 class Tdee extends React.Component {
+
+    submitHandler = (event) => {
+        event.preventDefault()
+        //console.log("hi")
+    }
+
     render () {
         return (
             <div>
                 <h1 className="Header"> Calorie and Macro Calculator </h1>
-                <form className="tdee-form">
+                <form onSubmit= {this.submitHandler} className="tdee-form">
                     <label>
                         Weight:
-                        <input className="tdee-inputs" type="text" placeholder="kg" />
+                        <input className="tdee-inputs" type="text" name="weight" placeholder="kg" />
                     </label>
                     <label>
                         Height:
-                        <input className="tdee-inputs" type="text" placeholder="cm" />
+                        <input className="tdee-inputs" type="text" name="height" placeholder="cm" />
                     </label>
                     <label>
                         Age:
-                        <input className="tdee-inputs" type="text" placeholder="years" />
+                        <input className="tdee-inputs" type="text" name="age" placeholder="years" />
                     </label>
                     <label>
                         Sex:
-                        <select>
+                        <select name="sex">
                             <option value="male">Male</option>
                             <option value="female">Female</option>
                         </select>
