5e9894a62f7b8b47993a6ba9a74bcc5976d988db
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-05-26 18:59:54 -0400

    added callback function and handlers to pull data and then send to calc function

--- src/components/Tdee.js
+++ src/components/Tdee.js
@@ -4,7 +4,23 @@ class Tdee extends React.Component {
 
     submitHandler = (event) => {
         event.preventDefault()
-        //console.log("hi")
+        let weight = this.state.weight
+        let height = this.state.height
+        let age = this.state.age
+        let sex = this.state.sex
+        let goal = this.state.goal
+        this.macroCalculator(weight, height, age, sex, goal);
+        //console.log(weight, height, age, sex, goal);
+    }
+
+    changeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({[nam]: val});
+    }
+
+    macroCalculator(weight, height, age, sex, goal) {
+        console.log(weight, height, age, sex, goal);
     }
 
     render () {
@@ -13,30 +29,30 @@ class Tdee extends React.Component {
                 <h1 className="Header"> Calorie and Macro Calculator </h1>
                 <form onSubmit= {this.submitHandler} className="tdee-form">
                     <label>
-                        Weight:
-                        <input className="tdee-inputs" type="text" name="weight" placeholder="kg" />
+                        Weight: 
+                        <input className="tdee-inputs" type="text" name="weight" placeholder="kg" onChange={this.changeHandler} />
                     </label>
                     <label>
                         Height:
-                        <input className="tdee-inputs" type="text" name="height" placeholder="cm" />
+                        <input className="tdee-inputs" type="text" name="height" placeholder="cm" onChange={this.changeHandler} />
                     </label>
                     <label>
                         Age:
-                        <input className="tdee-inputs" type="text" name="age" placeholder="years" />
+                        <input className="tdee-inputs" type="text" name="age" placeholder="years" onChange={this.changeHandler} />
                     </label>
                     <label>
                         Sex:
-                        <select name="sex">
+                        <select name="sex" onChange={this.changeHandler} >
                             <option value="male">Male</option>
                             <option value="female">Female</option>
                         </select>
                     </label>
                     <label>
                         Goal:
-                        <select name="goal">
-                            <option value="lose">lose weight</option>
-                            <option value="gain">gain weight</option>
-                            <option value="maintain">maintain weight</option>
+                        <select name="goal" onChange={this.changeHandler} >
+                            <option value="lose">Lose weight</option>
+                            <option value="gain">Gain weight</option>
+                            <option value="maintain">Maintain weight</option>
                         </select>
                     </label>
                     <input type="submit" value="Calculate Macros!" />
