7d1b1233a61d2a5514b5a98ef62f2bab3a1e33f7
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-05-28 13:52:08 -0400

    fetch call

--- src/components/myGoals.js
+++ src/components/myGoals.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class Mygoals extends React.Component {
+
+    componentDidMount() {
+        fetch("http://localhost:3000/api/v1/goals")
+        .then(response => response.json())
+        .then(
+            (result) => {
+                console.log(result)
+            }
+        )
+    }
+
     render () {
         return(
             <div>
