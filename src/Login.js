937a4965ba88d115ce501b5615a9a3515289205a
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-01 10:58:50 -0400

    changed login.js to a component

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,14 @@
-function Login() {
+import React from 'react';
+
+class Login extends React.Component {
+  render () {
     return (
       <div className="Login">
           hi
       </div>
     );
   }
+}
   
   export default Login;
   
