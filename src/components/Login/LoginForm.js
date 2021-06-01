455499518a451b18d0e4e8ab46c788276096558b
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-01 11:16:09 -0400

    created form for login

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
 class LoginForm extends React.Component {
+
+    submitHandler = (event) => {
+        event.preventDefault()
+    }
+
+    changeHandler = (event) => {
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({[nam]: val});
+    }
+
     render () {
       return (
         <div className="login">
-            In Login Form
+            <h1 className="Header">Login!</h1>
+            <form onSubmit= {this.submitHandler} className="tdee-form">
+                <label>
+                    Email:
+                    <input className="tdee-inputs" type="text" name="email" placeholder="email@gmail.com" onChange={this.changeHandler} />
+                </label>
+                <label>
+                    Password:
+                    <input className="tdee-inputs" type="password" name="password" placeholder="password" onChange={this.changeHandler} />
+                </label>
+                <input type="submit" value="Login" />
+            </form>
         </div>
       );
     }
