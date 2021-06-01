c4e9e24a8cac6c7f515643f40268196cdf65f4ec
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-01 11:45:09 -0400

    added headers

--- src/components/Login/Loginpage.js
+++ src/components/Login/Loginpage.js
@@ -6,6 +6,8 @@ class Loginpage extends React.Component {
   render () {
     return (
       <div>
+        <h1 className="Header">Welcome to myFoodJournal!</h1>
+        <h6 className="tdee-form">If you already have an account please login in the form below! If this is your first time with us please Sign up using the second form!</h6>
           <LoginForm />
           <SignUpForm />
       </div>
