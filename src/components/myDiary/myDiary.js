c4014765f6aee5ba62de0836390ef3e9ba745948
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-02 17:46:34 -0400

    added componentdidmount fetch call

--- src/components/myDiary/myDiary.js
+++ src/components/myDiary/myDiary.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 class Mydiary extends React.Component {
+
+    componentDidMount(){
+        let user_id = 1
+
+        fetch(`http://localhost:3000/api/v1/users/${user_id}`)
+        .then(response => response.json())
+        .then(
+            (result) => {
+                console.log(result)
+
+            }
+        )
+    }
+
     render () {
         return(
             <div>
