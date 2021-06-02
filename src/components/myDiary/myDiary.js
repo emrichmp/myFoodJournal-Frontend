b466965a015d09d8e7c79205c69cae866c93b1bd
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-02 18:58:20 -0400

    added today var

--- src/components/myDiary/myDiary.js
+++ src/components/myDiary/myDiary.js
@@ -3,14 +3,17 @@ import React from 'react';
 class Mydiary extends React.Component {
 
     componentDidMount(){
-        //let user_id = 1
+        let user_id = 1
 
         fetch(`http://localhost:3000/api/v1/journals`)
         .then(response => response.json())
         .then(
             (result) => {
-                console.log(result)
-
+                
+                let today = new Date().toISOString().slice(0, 10)
+                console.log(result.data[0].attributes.date)
+                console.log(user_id)
+                console.log(today)
             }
         )
     }
