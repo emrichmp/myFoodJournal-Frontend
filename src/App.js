038e37e02094933d0c88576c25bd2e85ffe3d43d
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-03 13:04:54 -0400

    moved logic for login to app

--- src/App.js
+++ src/App.js
@@ -9,21 +9,29 @@ import Tdee from "./components/Tdee"
 import Mydiary from './components/myDiary/myDiary';
 import Mygoals from './components/myGoals/myGoals';
 import myHistory from './components/myHistory';
+import Loginpage from './components/Login/Loginpage';
 
 function App() {
-  return (
-    <div className="App">
-      <HashRouter>
-        <Navbar />
-        <div className="content">
-          <Route exact path="/tdee" component={Tdee}/>
-          <Route exact path="/myDiary" component={Mydiary}/>
-          <Route exact path="/myGoals" component={Mygoals}/>
-          <Route exact path="/myHistory" component={myHistory}/>
-        </div>
-      </HashRouter>
-    </div>
-  );
+  let loggedIn = 0
+  if (loggedIn === 1) {
+    return (
+      <div className="App">
+        <HashRouter>
+          <Navbar />
+          <div className="content">
+            <Route exact path="/tdee" component={Tdee}/>
+            <Route exact path="/myDiary" component={Mydiary}/>
+            <Route exact path="/myGoals" component={Mygoals}/>
+            <Route exact path="/myHistory" component={myHistory}/>
+          </div>
+        </HashRouter>
+      </div>
+    );
+  } else {
+    return (
+      <Loginpage />
+    )
+  }
 }
 
 export default App;
