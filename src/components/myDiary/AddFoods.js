69ca2038f5d18b066771ac0fea4ac21d194f4c90
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-03 11:02:03 -0400

    added food form

--- src/components/myDiary/AddFoods.js
+++ src/components/myDiary/AddFoods.js
@@ -6,16 +6,16 @@ class AddFoods extends React.Component {
             <div>
                 <form>
                     <label>Food Name: </label>
-
+                    <input className="add-food" type="text" name="name" placeholder="ex: chicken" onChange={this.changeHandler} />
                     <label>Calories: </label>
-
+                    <input className="add-food" type="text" name="calories" placeholder="cals" onChange={this.changeHandler} />
                     <label>Protein: </label>
-
+                    <input className="add-food" type="text" name="protein" placeholder="g" onChange={this.changeHandler} />
                     <label>Fat: </label>
-
+                    <input className="add-food" type="text" name="fat" placeholder="g" onChange={this.changeHandler} />
                     <label>Carb: </label>
-
-                    <button> Add Food to Diary </button>
+                    <input className="add-food" type="text" name="carb" placeholder="g" onChange={this.changeHandler} />
+                    <button className="food-button"> Add Food to Diary </button>
                 </form>
             </div>
         )
