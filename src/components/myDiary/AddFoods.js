5c1f6dc1ab05320ec8999988450efd85d136a75b
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-07 11:05:12 -0400

    removed selection b/cit isnt necessary anymore

--- src/components/myDiary/AddFoods.js
+++ src/components/myDiary/AddFoods.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import FoodItem from "./FoodItem"
 
 class AddFoods extends React.Component {
 
@@ -14,17 +13,12 @@ class AddFoods extends React.Component {
     clickHandler = (event) => {
         event.preventDefault()
         console.log("in click")
-        let name = this.state.name
         let calories = this.state.calories
         let protein = this.state.protein
         let fat = this.state.fat
         let carb = this.state.carb
-        console.log(name, calories, protein, fat, carb)
+        console.log(calories, protein, fat, carb)
         //Update state to add food
-        // this.render (
-        //     <FoodItem name={this.state.name} calories={this.state.calories} protein={this.state.protein} 
-        //                 fat={this.state.fat} carb={this.state.carb}/>
-        // )
     }
 
     changeHandler = (event) => {
