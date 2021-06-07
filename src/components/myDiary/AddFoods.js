f2615d0847cd554122aee14aeb4099e1ed97f5de
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-07 11:16:27 -0400

    styling to addfoods

--- src/components/myDiary/AddFoods.js
+++ src/components/myDiary/AddFoods.js
@@ -31,17 +31,17 @@ class AddFoods extends React.Component {
         return(
             <div>
                 <button className="save-button">Save Diary!</button>
-                <form>
+                <form className="add-food">
                     <label>Food Name: </label>
-                    <input className="add-food" type="text" name="name" placeholder="ex: chicken" onChange={this.changeHandler} />
+                    <input className="add-food-inputs" type="text" name="name" placeholder="ex: chicken" onChange={this.changeHandler} />
                     <label>Calories: </label>
-                    <input className="add-food" type="text" name="calories" placeholder="cals" onChange={this.changeHandler} />
+                    <input className="add-food-inputs" type="text" name="calories" placeholder="cals" onChange={this.changeHandler} />
                     <label>Protein: </label>
-                    <input className="add-food" type="text" name="protein" placeholder="g" onChange={this.changeHandler} />
+                    <input className="add-food-inputs" type="text" name="protein" placeholder="g" onChange={this.changeHandler} />
                     <label>Fat: </label>
-                    <input className="add-food" type="text" name="fat" placeholder="g" onChange={this.changeHandler} />
+                    <input className="add-food-inputs" type="text" name="fat" placeholder="g" onChange={this.changeHandler} />
                     <label>Carb: </label>
-                    <input className="add-food" type="text" name="carb" placeholder="g" onChange={this.changeHandler} />
+                    <input className="add-food-inputs" type="text" name="carb" placeholder="g" onChange={this.changeHandler} />
                     <button className="food-button" onClick={this.clickHandler} > Add Food to Diary </button>
                 </form>
             </div>
