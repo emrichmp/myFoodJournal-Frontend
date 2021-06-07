ace5c9545116154124678e398333d8df68d36b58
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-07 15:43:13 -0400

    added - after food name

--- src/components/myDiary/FoodItem.js
+++ src/components/myDiary/FoodItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 class FoodItem extends React.Component {
     render () {
         return(
-            <div>{this.props.name} {this.props.calories} cals {this.props.protein} Protein {this.props.fat} Fat {this.props.carb} Carbs</div>
+            <div>{this.props.name} - {this.props.calories} cals {this.props.protein} Protein {this.props.fat} Fat {this.props.carb} Carbs</div>
         )
     }
 }
