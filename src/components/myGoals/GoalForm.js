c9bebeeb66f5ea97b3ee884e5c2375f1ba14162d
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-08 09:26:06 -0400

    removed comment

--- src/components/myGoals/GoalForm.js
+++ src/components/myGoals/GoalForm.js
@@ -26,7 +26,6 @@ class GoalForm extends React.Component {
     }
 
     patchHandler(goalweight, goalcalories, goalprotein, goalfats, goalcarbs) {
-        //HAVE TO CHANGE THIS SO IT IS THE ACTUAL Goal ID
         const goal_id = this.props.goal_id
 
         fetch(`http://localhost:3000/api/v1/goals/${goal_id}`, {
